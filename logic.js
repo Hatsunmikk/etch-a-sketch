6e7673b81d23e25095b4daf6e367deac0d4522e7
Hatsunmikk <debos2003@gmail.com>
2024-12-12 13:02:52 +0530

    Add a function called createGrid() that will dynamically size the squares to fit within the grid container according to the number of squares the user asks for

--- logic.js
+++ logic.js
@@ -1,24 +1,28 @@
 
 
 
-function createGrid (){
+function createGrid (numOfSquares){
 
-    
+    //Main grid container which will contain all the square within it
+     const gridContainer = document.getElementById("grid-container");
 
-}
-//Main grid container which will contain all the square within it
-const gridContainer = document.getElementById("grid-container");
+     //Clearing the existing grid (if any)
+     gridContainer.innerHTML = "";
 
+     // Calculate the size of each square based on the number of squares per side
+     const squareSize = 960 / numOfSquares;
 
-
-//A loop to create the squares
-for(let i = 0; i < 256; i++){
+     //A loop to create the squares
+    for(let i = 0; i < numOfSquares * numOfSquares; i++){
 
     //creating each square as a div
     const square = document.createElement("div");
      
     //each created square is being assigned a class to target it for styling
     square.classList.add("grid-square");
+
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
      
     //Adding hover effects to create a trail 
     square.addEventListener("mouseover", () =>
@@ -37,6 +41,9 @@ for(let i = 0; i < 256; i++){
     gridContainer.appendChild(square);
 };
 
+};
+
+
 const button = document.getElementById("btn");
 
 //How clicking the button guides us to the function that creates the grid
@@ -44,13 +51,16 @@ button.addEventListener("click", () => {
    let numOfSquares = prompt("How many squares do you want? (max. 100)");
    numOfSquares = parseInt(numOfSquares);
 
-   if(isNaN(numOfSquares) || numOfSquares < 1 || numOfSquares >100){
+   if(isNaN(numOfSquares) || numOfSquares < 1 || numOfSquares > 100){
        alert("Please enter a valid number between 1 an 100.");
        return;
    };
 
    createGrid(numOfSquares);
 });
+
+//Creating a default grid on page-load
+window.onload = () => createGrid(16);
 
 
 
